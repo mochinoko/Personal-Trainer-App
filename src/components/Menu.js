ec5dbc366f939ae6d5c399a07ae78bff5ccd4c5f
Yuki Sugiyama <35254255+mochinoko@users.noreply.github.com>
2020-11-19 17:11:06 +0200

    added date picker dependencies

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import Training from './Training';
 import CustomerList from './CustomerList';
+import Calendar from './Calendar';
 
  function Menu() {
 
@@ -12,10 +13,14 @@ import CustomerList from './CustomerList';
             <Link to="/home" > Home </Link>
             
             <Link to="/training"> Training </Link>
+
+            <Link to="/calendar"> Calendar </Link>
             <Switch>
                 <Route exact path="/home" component={CustomerList} />
               
-                <Route path="/training" component={Training} margin="auto" />
+                <Route path="/training" component={Training}  />
+
+                <Route path="/calendar" component={Calendar} />
         
                 <Route render={() => <h1>Page not found</h1>} />
             </Switch>
